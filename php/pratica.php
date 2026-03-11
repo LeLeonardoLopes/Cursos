e20183a53d74748695e4b7d1359cde4db526fd19
Leonardo Lopes <leleonardolopes@gmail.com>
2026-03-11 15:01:26 -0300

    Att

---
+++ php/pratica.php
@@ -0,0 +1,39 @@
+<?php
+
+    echo "Meu nome é Leonardo \n" ;
+
+    echo str_repeat("-", 50), "\n";
+
+///////////////////////////////////////////    
+    
+$nota1 = 2.0;
+$nota2 = 4.0;
+$nota3 = 6.0;
+
+$medianotas = ($nota1 + $nota2 + $nota3) /3 ;
+
+echo "A média das notas é: " .$medianotas ."\n";
+
+echo str_repeat("-", 50 ), "\n";
+
+///////////////////////////////////////////////////////
+
+$metros = 100;
+
+$conversaoParaCm = $metros *100;
+
+echo "A conversão é: " .$conversaoParaCm , "\n";
+
+echo str_repeat("-", 50 ), "\n";
+
+//////////////////////////////////////////////////////
+
+$celsius = 30;
+
+$fahrenheit = ($celsius * 1.8) + 32;
+
+echo "A conversão de celsius para Fahrenheit é: " .$fahrenheit, "\n";
+
+echo str_repeat("-", 50 ), "\n";
+
+//////////////////////////////////////////////////////
---
+++ php/screen-match.php
@@ -0,0 +1,31 @@
+<?php 
+
+echo "Bem vindo ao screen Match!\n";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = $argv[1] ?? 2022;
+
+$somaDeNotas = 9 ;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5; 
+
+$notaFilme = $somaDeNotas/ 5;
+$planoPrime = true;
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo "Nome do filme: " .$nomeFilme ."\n"; 
+echo "Nota do filme: " .$notaFilme ."\n";
+echo "Ano de lançamento: " .$anoLancamento ."\n";
+
+$textoIncluido = $incluidoNoPlano ? "Sim" : "Não";
+echo "Incluído no plano: $textoIncluido\n";
+
+if ($anoLancamento >= 2022) {
+    echo "Esse filme é um lançamento.\n";
+} else {
+    echo "Esse filme não é um lançamento.\n";
+}
+
+echo str_repeat("-", 50 ), "\n";
